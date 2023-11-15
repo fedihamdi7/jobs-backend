11c480522b0ed4def5959c27ecc50735a7d52f4a
Fedi Hamdi <fedihamdi97@gmail.com>
2023-11-15 03:43:33 +0100

    remove log

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
-import { log } from 'console';
 
 @Injectable()
 export class AuthService {
