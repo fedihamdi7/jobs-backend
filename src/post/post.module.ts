d5c4838b6917e78cbc848d05d18cce9ad50ed152
Fedi Hamdi <fedihamdi97@gmail.com>
2023-11-17 13:52:01 +0100

    fix wrong schema

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -3,12 +3,13 @@ import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostSchema } from './entities/post.entity';
+import { UserSchema } from 'src/user/entities/user.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Post', schema: PostSchema },
-      { name: 'User', schema: PostSchema}
+      { name: 'User', schema: UserSchema }
     ])
   ],
   controllers: [PostController],
