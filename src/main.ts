d6ff65fabc5c8cabee6c86591e15234fd644bc8d
Fedi Hamdi <fedihamdi97@gmail.com>
2023-11-22 17:18:15 +0100

    let frontEnd access assets folder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as serveStatic from 'serve-static';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('/assets',serveStatic('assets'));
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors(
